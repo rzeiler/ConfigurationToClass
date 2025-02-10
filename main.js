09eeac3c2a52330ef08c31ee106656033340f42e
Ralf Zeiler <rze.entwicklung@gmail.com>
2025-02-10 15:55:14 +0100

    path service worker

--- main.js
+++ main.js
@@ -242,7 +242,7 @@ createApp({
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register("https://rzeiler.github.io/ConfigurationToClass/service-worker.js")
+      .register("/service-worker.js")
       .then((registration) => {
         console.log(
           "Service Worker registered with scope:",
