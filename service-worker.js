0b8a09ed075341540192c00674652c6b62b27d07
Ralf Zeiler <rze.entwicklung@gmail.com>
2025-02-10 16:11:28 +0100

    sw

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,11 @@
 const CACHE_NAME = "pwa-cache-v2";
 const ASSETS_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/manifest.json",
-  "/service-worker.js",
-  "/favicon.ico",
-  "/main.js", // Your Vue application script
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./service-worker.js",
+  "./favicon.ico",
+  "./main.js", // Your Vue application script
 ];
 
 // Install event: Cache files for offline use
